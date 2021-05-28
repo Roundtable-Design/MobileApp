d03072f887d262bf96f9a5ef4aa4c00d86d5648b
Gil <gilbertjvirgo@gmail.com>
2021-05-28 16:28:11 +0100

    Added upload link

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 
+import { Link } from "react-router-native";
 import React from "react";
 
 export default function Home() {
 	return (
 		<View style={styles.container}>
 			<Text>This is Home!</Text>
+
+			<Link to="/upload">
+				<Text>Go to upload page</Text>
+			</Link>
 		</View>
 	);
 }
